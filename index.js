8ccc64af26d247857286f21e76cabb8c8e4f4fdb
nirmal100-ux <nirmalstha783@gmail.com>
2023-08-23 09:36:24 +0545

    change

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auths.js";
 import tweetRoutes from "./routes/tweets.js";
 
 const app = express();
 dotenv.config();
-const cors = require('cors');
+
 
 const connect = () => {
   mongoose.set("strictQuery", false);
